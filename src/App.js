67725667c109be3c29bf8709aafbb0734b300ba9
fremartini <frederik.martini@live.dk>
2022-10-31 19:54:17 +0100

    add icons

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
-import { useEffect } from 'react';
-import './App.css';
+import google from './assets/google.png'
+import apple from './assets/apple.png'
+import djuke from './assets/djuke.png'
 
 function App() {
-  useEffect(() => {
-    document.title = "DJuke"
-  })
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <p style={{
-          color: "#FFFFFF"
+    <div style={{
+      backgroundColor: "#FF61CD",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      minHeight: "100vh",
+    }}>
+      <img src={djuke} alt="djuke" />
+      <div style={{
+        marginBottom: "2rem",
+      }} />
+      <div style={{
+        display: "flex",
+        flexDirection: "row"
+      }}>
+        <a href='http://www.djuke.dk'>
+          <img src={google} alt="google play" height="40" width="120" />
+        </a>
+        <div style={{
+          marginLeft: "4px",
+          marginRight: "4px"
+        }} />
+        <a href='http://www.djuke.dk'>
+          <img src={apple} alt="app store" />
+        </a>
+      </div>
+      <footer style={{
+        position: "absolute",
+        bottom: 20,
+      }}>
+        <div style={{
+          display: "flex",
+          flexDirection: "row",
         }}>
-          This webpage is under construction
-        </p>
-      </header>
+          <a style={{
+            textDecoration: "none",
+            color: "#FFFFFF",
+          }} href='http://www.djuke.dk'>Legal</a>
+          <div style={{
+            marginLeft: "16px",
+            marginRight: "16px"
+          }} />
+          <a style={{
+            textDecoration: "none",
+            color: "#FFFFFF",
+          }} href='http://www.djuke.dk'>Help</a>
+        </div>
+      </footer>
     </div>
   );
 }
